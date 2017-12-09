6329553068da9672e46e12f61914cc3e961e9611
Ilia <ivv.evol@gmail.com>
2017-12-09 20:34:25 +0300

    add: TrafficLight project

---
+++ TrafficLight/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TrafficLight
+{
+    public abstract class TrafficLight
+    {
+        protected string color;
+        
+        public TrafficLight()
+        {
+            color = "red";
+        }
+
+        public virtual void Run(int switchTime)
+        {
+            // start to switch states periodically 
+        }
+
+        public string getState()
+        {
+            return color;
+        }
+        
+    }
+    
+    class AutoTrafficLight : TrafficLight
+    {
+        public override void Run(int switchTime)
+        {
+            color = "red";
+        }
+    }
+    
+    class TrainTrafficLight : TrafficLight
+    {
+        public override void Run(int switchTime)
+        {
+            color = "green";
+        }
+    }
+
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+        }
+
+        public virtual bool moveAllowed(TrafficLight v)
+        {
+            return false;
+        }
+    }
+    
+    class Auto : Vehicle
+    {
+        public override bool moveAllowed(TrafficLight v)
+        {
+            // ...
+            return false;
+        }
+    }
+    
+    class Train : Vehicle
+    {
+        public override bool moveAllowed(TrafficLight v)
+        {
+            // ...
+            return false;
+        }
+    }
+}
